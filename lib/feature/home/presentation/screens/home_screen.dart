8c895cc8d9bace5ab86c40e1d2ec9478769729d6
Mohammed Navas <navaskt38@gmail.com>
2025-08-27 17:38:01 +0400

    Add Riverpod joke feature and update iOS target
    Introduces a new joke feature using Riverpod, including a Joke model and a HomeView screen. Updates main.dart to use Riverpod and fetch jokes from an API. Raises iOS minimum deployment target to 13.0 and updates Xcode scheme for debugging. Adds Dio and Riverpod dependencies, and cleans up .gitignore.

---
+++ lib/feature/home/presentation/screens/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiverpod_sample_flutter/main.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sample Riverpod Application')),
+      body: SizedBox.expand(
+        child: Consumer(
+          builder: (context, ref, _) {
+            final randomJoke = ref.watch(randomJokeProvider);
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                if (randomJoke.isRefreshing)
+                  const Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: LinearProgressIndicator(),
+                  ),
+
+                switch (randomJoke) {
+                  AsyncValue(:final value?) => SelectableText(
+                    '${value.setup}\n\n${value.punchline}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  AsyncValue(error: != null) => const Text(
+                    'Error fetching joke',
+                  ),
+                  AsyncValue() => const CircularProgressIndicator(),
+                },
+
+                Positioned(
+                  bottom: 20,
+                  child: ElevatedButton(
+                    onPressed: () => ref.invalidate(randomJokeProvider),
+                    child: const Text('Get another joke'),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}

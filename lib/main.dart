7c950a2edfb35e59af3cddc61a83a9f95c7590a7
Mohammed Navas <navaskt38@gmail.com>
2025-08-28 14:31:34 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:dio/dio.dart';
-
-import 'feature/home/data/models/joke_model.dart';
 import 'feature/home/presentation/screens/home_screen.dart';
 
 void main() {
@@ -18,19 +15,3 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(home: HomeView());
   }
 }
-
-final dio = Dio();
-
-Future<Joke> fetchRandomJoke() async {
-  // Fetching a random joke from a public API
-  final response = await dio.get<Map<String, Object?>>(
-    'https://official-joke-api.appspot.com/random_joke',
-  );
-
-  return Joke.fromJson(response.data!);
-}
-
-final randomJokeProvider = FutureProvider<Joke>((ref) async {
-  // Using the fetchRandomJoke function to get a random joke
-  return fetchRandomJoke();
-});
